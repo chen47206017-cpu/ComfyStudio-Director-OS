94390e14d9839c27d6f4bdc897ef39f8cf9805d5
chen47206017-cpu <chen47206017-cpu@users.noreply.github.com>
2026-09-08 13:32:47 +0800

    feat: add V10.4.3 character asset lock engine

---
+++ ComfyStudio_StudioOS_CURRENT/frontend/src/director_os/engines/asset_engine.ts
@@ -0,0 +1,45 @@
+﻿
+
+import {
+
+validateCharacterAsset
+
+}
+
+from "../asset_engine/validator";
+
+
+
+export class AssetEngine{
+
+
+validate(
+
+id:string,
+
+year:number,
+
+age:number
+
+){
+
+
+return validateCharacterAsset(
+
+id,
+
+year,
+
+age
+
+);
+
+
+}
+
+
+
+}
+
+
+
