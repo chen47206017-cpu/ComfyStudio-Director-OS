1d81675fb0ae6e0a6976fb863e2dac762f3f1469
chen47206017-cpu <chen47206017-cpu@users.noreply.github.com>
2026-09-08 13:50:50 +0800

    feat: create auto director pipeline foundation v13.0

---
+++ ComfyStudio_StudioOS_CURRENT/frontend/src/director_os/auto_director/auto_director.ts
@@ -0,0 +1,55 @@
+﻿
+
+import {
+
+analyzeScript
+
+}
+
+from "./script_analyzer";
+
+
+
+import {
+
+generateShots
+
+}
+
+from "./shot_generator";
+
+
+
+
+export class AutoDirector{
+
+
+createProject(input:any){
+
+
+return analyzeScript(input);
+
+
+}
+
+
+
+
+createShots(
+episode:any
+){
+
+
+return generateShots(
+episode
+);
+
+
+}
+
+
+
+}
+
+
+
---
+++ ComfyStudio_StudioOS_CURRENT/frontend/src/director_os/auto_director/script_analyzer.ts
@@ -0,0 +1,52 @@
+﻿
+
+import {
+
+ScriptProject
+
+}
+
+from "./schema";
+
+
+
+
+export function analyzeScript(
+input:any
+){
+
+
+
+const project:ScriptProject={
+
+
+id:
+"PROJECT_"+Date.now(),
+
+
+title:
+input.title,
+
+
+description:
+input.text,
+
+
+episodes:[
+
+]
+
+
+
+};
+
+
+
+return project;
+
+
+
+}
+
+
+
