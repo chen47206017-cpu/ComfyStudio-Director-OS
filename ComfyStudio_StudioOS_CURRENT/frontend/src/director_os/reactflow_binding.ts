9bf9751d7fd1444a91a9025c24678c75b141cdbb
chen47206017-cpu <chen47206017-cpu@users.noreply.github.com>
2026-09-08 13:48:57 +0800

    feat: bind director workflow to reactflow v12.4

---
+++ ComfyStudio_StudioOS_CURRENT/frontend/src/director_os/reactflow_binding.ts
@@ -0,0 +1,47 @@
+﻿
+
+import {
+defaultReactFlowNodes
+}
+from "./default_reactflow_nodes";
+
+
+import {
+defaultReactFlowEdges
+}
+from "./default_reactflow_edges";
+
+
+import {
+directorReactFlowNodeTypes
+}
+from "./reactflow_node_types";
+
+
+
+export function loadDirectorWorkflow(){
+
+
+return {
+
+
+nodes:
+defaultReactFlowNodes,
+
+
+edges:
+defaultReactFlowEdges,
+
+
+nodeTypes:
+directorReactFlowNodeTypes
+
+
+
+};
+
+
+}
+
+
+
---
+++ ComfyStudio_StudioOS_CURRENT/frontend/src/director_os/reactflow_node_types.ts
@@ -0,0 +1,39 @@
+﻿
+import ScriptNode from "./nodes/ScriptNode";
+import CanonNode from "./nodes/CanonNode";
+import AssetNode from "./nodes/AssetNode";
+import ReferenceNode from "./nodes/ReferenceNode";
+import PromptNode from "./nodes/PromptNode";
+import ModelNode from "./nodes/ModelNode";
+import QCNode from "./nodes/QCNode";
+import DeliveryNode from "./nodes/DeliveryNode";
+
+
+export const directorReactFlowNodeTypes={
+
+
+SCRIPT:ScriptNode,
+
+CANON:CanonNode,
+
+CHARACTER:AssetNode,
+
+SCENE:AssetNode,
+
+PROP:AssetNode,
+
+REFERENCE:ReferenceNode,
+
+PROMPT:PromptNode,
+
+MODEL:ModelNode,
+
+QC:QCNode,
+
+DELIVERY:DeliveryNode
+
+
+
+};
+
+
