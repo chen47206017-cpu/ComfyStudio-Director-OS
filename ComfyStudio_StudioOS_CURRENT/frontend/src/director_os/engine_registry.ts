2262f419d82f7dbb2431d30227e40d0097ea6af1
chen47206017-cpu <chen47206017-cpu@users.noreply.github.com>
2026-09-08 13:47:37 +0800

    feat: bind director runtime to real engines v12.1

---
+++ ComfyStudio_StudioOS_CURRENT/frontend/src/director_os/engine_registry.ts
@@ -0,0 +1,44 @@
+﻿
+import {CanonEngine} from "./canon_engine";
+import {SceneEngine} from "./scene_engine";
+import {PropEngine} from "./prop_engine";
+import {ReferenceEngine} from "./reference_engine";
+import {PromptEngine} from "./prompt_engine";
+import {ModelRouterEngine} from "./model_router_engine";
+import {QCEngine} from "./qc_engine";
+
+
+export const engineRegistry={
+
+
+CANON:
+new CanonEngine(),
+
+
+SCENE:
+new SceneEngine(),
+
+
+PROP:
+new PropEngine(),
+
+
+REFERENCE:
+new ReferenceEngine(),
+
+
+PROMPT:
+new PromptEngine(),
+
+
+MODEL:
+new ModelRouterEngine(),
+
+
+QC:
+new QCEngine()
+
+
+};
+
+
--- ComfyStudio_StudioOS_CURRENT/frontend/src/director_os/runtime_controller.ts
+++ ComfyStudio_StudioOS_CURRENT/frontend/src/director_os/runtime_controller.ts
@@ -1,10 +1,15 @@
 ﻿
+import {
+engineRegistry
+}
+from "./engine_registry";
+
+
 
 export class DirectorRuntime{
 
 
 async execute(workflow:any){
-
 
 
 for(
@@ -15,15 +20,61 @@ const node of workflow.nodes
 node.status="RUNNING";
 
 
+try{
 
-// TODO connect engines
+
+const engine:any=
+(engineRegistry as any)[node.type];
+
+
+
+if(engine){
+
+
+const result=
+await engine.check?
+engine.check(node.data)
+:
+engine.run?
+engine.run(node.data)
+:
+engine.select?
+engine.select(node.data)
+:
+engine.build?
+engine.build(node.data)
+:
+null;
+
+
+
+node.data.result=result;
+
+
+
+}
+
 
 
 node.status="SUCCESS";
 
 
 }
+catch(error){
 
+
+node.status="FAILED";
+
+
+node.data.error=
+String(error);
+
+
+}
+
+
+
+}
 
 
 return workflow;
@@ -34,6 +85,5 @@ return workflow;
 
 
 }
-
 
 
